b1861ac531ef8830a589c52cc98bc3247a6af7a3
Mahim <mahim.g03@gmail.com>
2023-08-21 02:47:04 +0530

    Added Decaying Nature

--- server/contracts/LoyaltyToken.sol
+++ server/contracts/LoyaltyToken.sol
@@ -2,15 +2,11 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-
-// import "hardhat/console.sol";
 
 contract LoyaltyToken is ERC20, ERC20Burnable, AccessControl {
     using SafeMath for uint256;
@@ -23,14 +19,13 @@ contract LoyaltyToken is ERC20, ERC20Burnable, AccessControl {
     address private _contractDeployer;
     uint256 private interval;
 
-    function decimals() public view virtual override returns (uint8) {
-        return 0;
-    }
+    mapping(uint256 => transaction) private transactionLog;
+    mapping(address => uint256) public balanceFor;
 
     constructor() ERC20("Loyalty Tokens", "DE") {
         _contractDeployer = msg.sender;
         interval = 500;
-        _mint(msg.sender, 100000000 * decimals());
+        _mint(msg.sender, 1000000000 * decimals());
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
@@ -54,10 +49,6 @@ contract LoyaltyToken is ERC20, ERC20Burnable, AccessControl {
     );
     event fetchAll(uint256 totalAmount, transaction[]);
 
-    mapping(uint256 => transaction) private transactionLog;
-    // balanceOf
-    mapping(address => uint256) public balanceFor;
-
     modifier isAdmin() {
         require(
             (hasRole(ADMIN_ROLE, msg.sender) ||
@@ -65,6 +56,10 @@ contract LoyaltyToken is ERC20, ERC20Burnable, AccessControl {
             "You don't have admin rights!"
         );
         _;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
     }
 
     modifier canMint() {
@@ -75,15 +70,6 @@ contract LoyaltyToken is ERC20, ERC20Burnable, AccessControl {
             "You don't have the rights to mint Tokens"
         );
         _;
-    }
-
-    function mint(uint256 amount) external returns (bool) {
-        require(
-            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
-            "Only Default Admin can create a supply"
-        );
-        _mint(msg.sender, amount * 10 ** decimals());
-        return true;
     }
 
     function mintTo(
@@ -161,49 +147,6 @@ contract LoyaltyToken is ERC20, ERC20Burnable, AccessControl {
         return true;
     }
 
-    function validateTokens(uint256 transactionId) public returns (bool) {
-        require(
-            transactionLog[transactionId].isCredit,
-            "Only for Credited Tokens"
-        );
-        uint256 currTime = block.timestamp;
-        uint256 mintTime = transactionLog[transactionId].mintDate;
-        address from = transactionLog[transactionId].customer;
-        uint256 amount = transactionLog[transactionId].amount;
-        uint256 currAmount = transactionLog[transactionId].currAmount;
-
-        if ((currTime > mintTime + interval) && (currAmount > 0)) {
-            bool res = burnToken(from, amount);
-            if (res) {
-                _transactionId.increment();
-                uint256 newTransId = _transactionId.current();
-                balanceFor[from] -= currAmount;
-                transactionLog[newTransId] = transaction(
-                    newTransId,
-                    from,
-                    amount,
-                    currAmount,
-                    0,
-                    false,
-                    "Tokens Expired"
-                );
-                emit tokenMinted(
-                    newTransId,
-                    from,
-                    amount,
-                    currAmount,
-                    0,
-                    false,
-                    "Tokens Expired"
-                );
-            } else revert("Tokens not Burned");
-
-            return false;
-        }
-
-        return true;
-    }
-
     function fetchTransactions(
         address _customer
     ) public view returns (transaction[] memory) {
@@ -228,62 +171,6 @@ contract LoyaltyToken is ERC20, ERC20Burnable, AccessControl {
         }
 
         return log;
-    }
-
-    // function fetchValidTransactions(
-    //     address _customer
-    // ) public returns (uint256) {
-    //     transaction[] memory allTransactions = fetchTransactions(_customer);
-    //     uint256 validTransCount = 0;
-    //     uint256 totalAmount = 0;
-    //     for (uint256 i = 0; i < allTransactions.length; i++) {
-    //         if (validateTokens(allTransactions[i].transactionId)) {
-    //             validTransCount++;
-    //         }
-    //     }
-
-    //     transaction[] memory validTransactions = new transaction[](
-    //         validTransCount
-    //     );
-    //     uint256 currIndex = 0;
-
-    //     for (uint256 i = 0; i < allTransactions.length; i++) {
-    //         if (validateTokens(allTransactions[i].transactionId)) {
-    //             validTransactions[currIndex] = allTransactions[i];
-    //             currIndex++;
-    //             if (validTransactions[currIndex].isCredit)
-    //                 totalAmount += (validTransactions[currIndex].currAmount);
-    //         }
-    //     }
-
-    //     emit fetchAll(totalAmount, validTransactions);
-    //     return totalAmount;
-    // }
-
-    function assignSeller(address _to) public isAdmin {
-        _grantRole(SELLER_ROLE, _to);
-    }
-
-    function removeSeller(address _from) public isAdmin {
-        require(hasRole(SELLER_ROLE, _from), "Seller doesn't exists");
-        _revokeRole(SELLER_ROLE, _from);
-    }
-
-    function assignAdmin(address _to) public {
-        require(
-            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
-            "Only Default Admin can assign Admin Rights"
-        );
-        _grantRole(ADMIN_ROLE, _to);
-    }
-
-    function removeAdmin(address _from) public {
-        require(hasRole(ADMIN_ROLE, _from), "Admin rights doesn't exists");
-        require(
-            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
-            "Only Default Admin can revoke Admin Rights"
-        );
-        _revokeRole(ADMIN_ROLE, _from);
     }
 
     function redeemTokens(uint256 amount) public returns (bool) {
@@ -315,21 +202,77 @@ contract LoyaltyToken is ERC20, ERC20Burnable, AccessControl {
             false,
             "Tokens Redeemed"
         );
-        // transaction[] memory all = fetchTransactions(msg.sender);
         balanceFor[msg.sender] -= amount;
+        return true;
+    }
 
-        // for (uint i = 0; i < all.length; i++) {
-        //     if (all[i].isCredit) {
-        //         if (amount >= all[i].currAmount) {
-        //             amount -= all[i].currAmount;
-        //             all[i].currAmount = 0;
-        //         } else {
-        //             all[i].currAmount -= amount;
-        //             amount = 0;
-        //             break;
-        //         }
-        //     }
-        // }
+    function assignSeller(address _to) public isAdmin {
+        _grantRole(SELLER_ROLE, _to);
+    }
+
+    function removeSeller(address _from) public isAdmin {
+        require(hasRole(SELLER_ROLE, _from), "Seller doesn't exists");
+        _revokeRole(SELLER_ROLE, _from);
+    }
+
+    function assignAdmin(address _to) public {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "Only Default Admin can assign Admin Rights"
+        );
+        _grantRole(ADMIN_ROLE, _to);
+    }
+
+    function removeAdmin(address _from) public {
+        require(hasRole(ADMIN_ROLE, _from), "Admin rights doesn't exists");
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "Only Default Admin can revoke Admin Rights"
+        );
+        _revokeRole(ADMIN_ROLE, _from);
+    }
+
+    function validateTokens(uint256 transactionId) public returns (bool) {
+        require(
+            transactionLog[transactionId].isCredit,
+            "Only for Credited Tokens"
+        );
+        uint256 currTime = block.timestamp;
+        uint256 mintTime = transactionLog[transactionId].mintDate;
+        address from = transactionLog[transactionId].customer;
+        uint256 amount = transactionLog[transactionId].amount;
+        uint256 currAmount = transactionLog[transactionId].currAmount;
+
+        if ((currTime > mintTime + interval) && (currAmount > 0)) {
+            bool res = burnToken(from, amount);
+            uint256 time = block.timestamp;
+            if (res) {
+                _transactionId.increment();
+                uint256 newTransId = _transactionId.current();
+                balanceFor[from] -= currAmount;
+                transactionLog[newTransId] = transaction(
+                    newTransId,
+                    from,
+                    amount,
+                    currAmount,
+                    time,
+                    false,
+                    "Tokens Expired"
+                );
+                emit tokenMinted(
+                    newTransId,
+                    from,
+                    amount,
+                    currAmount,
+                    time,
+                    false,
+                    "Tokens Expired"
+                );
+            } else revert("Tokens not Burned");
+
+            return false;
+        }
+
         return true;
     }
 }
